ea6072e4612bc479a2de5edde80da2c0d5b8bb82
Mark Klingelhoefer <mkling5577@gmail.com>
2023-10-18 21:06:33 -0400

    Edit playRound()
    Edited playRound so that it returns a message to the player, stating if they won or not and how they won or lost

--- script.js
+++ script.js
@@ -23,20 +23,28 @@ const playRound = (playerSelection, computerSelection) => {
     // console.log(`CPU choice: ${computerSelection}`)
     if (playerSelection === "rock" && computerSelection === "paper") {
         cpuScore ++;
+        return "You lose! Paper beats rock."
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         userScore ++;
+        return "You win! Rock beats scissors."
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         userScore ++;
+        return "You win! Paper beats rock."
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         cpuScore ++;
+        return "You lose! Scissors beats paper."
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         cpuScore ++;
+        return "You lose! Rock beats scissors."
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         userScore ++;
+        return "You win! Scissors beats paper."
     } else {
         userScore += 0;
         cpuScore += 0;
+        return "Tie!"
     }
 }
+
 
 // Report winner and loser after 5 games
