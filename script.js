e5a0219e19b2ea78289c86155c74448ca2688dc0
Mark Klingelhoefer <mkling5577@gmail.com>
2023-10-18 20:51:19 -0400

    Get user input

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ const getComputerChoice = () => {
         return "scissors"
     }
 }
+
 // Get user choice
+let userChoice = prompt("Enter rock, paper, or scissors: ").toLowerCase();
 
 // Play a single round of rock paper scissors
+
 
 // Report winner and loser after 5 games
