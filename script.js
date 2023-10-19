dfcee10eb977fa47984549798b82b03562390e66
Mark Klingelhoefer <mkling5577@gmail.com>
2023-10-18 20:57:17 -0400

    Add playRound() function
    Take the user choice and cpu choice and add 1 to score of whoever wins

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Initialize scores
+let userScore = 0;
+let cpuScore = 0;
+
 // Get computer choice
 const getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -14,6 +18,23 @@ const getComputerChoice = () => {
 let userChoice = prompt("Enter rock, paper, or scissors: ").toLowerCase();
 
 // Play a single round of rock paper scissors
-
+const playRound = (user, cpu) => {
+    if (user === "rock" && cpu === "paper") {
+        cpuScore ++;
+    } else if (user === "rock" && cpu === "scissors") {
+        userScore ++;
+    } else if (user === "paper" && cpu === "rock") {
+        userScore ++;
+    } else if (user === "paper" && cpu === "scissors") {
+        cpuScore ++;
+    } else if (user === "scissors" && cpu === "rock") {
+        cpuScore ++;
+    } else if (user === "scissors" && cpu === "paper") {
+        userScore ++;
+    } else {
+        userScore += 0;
+        cpuScore += 0;
+    }
+}
 
 // Report winner and loser after 5 games
